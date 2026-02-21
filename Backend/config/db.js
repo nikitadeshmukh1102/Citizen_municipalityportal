7cb5947059df3f4ca8285db0c0b4934e4b3285bd
nikitadeshmukh1102 <103282486+nikitadeshmukh1102@users.noreply.github.com>
2026-02-22 03:28:55 +0530

    Added MYSQL_URL config

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -9,7 +9,7 @@ db.connect((err) => {
   if (err) {
     console.error('MySQL connection failed:', err.message);
   } else {
-    console.log('✅ MySQL connected successfully');
+    console.log('MySQL connected successfully');
   }
 });
 
